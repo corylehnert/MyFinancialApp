3290b7d10fe07e15f2a5fed3e7b86a59e91cf6d3
Cory Lehnert <lehnertcory@gmail.com>
2024-08-07 18:45:17 -0500

    Add functionality to retrieve entries from local database and display them on screen.

--- MyFinancialApp/CreateFinancialReport.Designer.cs
+++ MyFinancialApp/CreateFinancialReport.Designer.cs
@@ -36,19 +36,20 @@
             this.lastPayHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.comBoxFrequency = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(468, 484);
-            this.btnGenerate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerate.Location = new System.Drawing.Point(351, 393);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(100, 28);
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
             this.btnGenerate.TabIndex = 0;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
-            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_ClickAsync);
             // 
             // listDebts
             // 
@@ -57,12 +58,11 @@
             this.amtHeader,
             this.nextPayHeader,
             this.lastPayHeader});
-            listDebts.GridLines = true;
+            this.listDebts.GridLines = true;
             this.listDebts.HideSelection = false;
-            this.listDebts.Location = new System.Drawing.Point(612, 65);
-            this.listDebts.Margin = new System.Windows.Forms.Padding(4);
+            this.listDebts.Location = new System.Drawing.Point(339, 25);
             this.listDebts.Name = "listDebts";
-            this.listDebts.Size = new System.Drawing.Size(351, 303);
+            this.listDebts.Size = new System.Drawing.Size(390, 247);
             this.listDebts.TabIndex = 1;
             this.listDebts.UseCompatibleStateImageBehavior = false;
             this.listDebts.View = System.Windows.Forms.View.Details;
@@ -88,38 +88,58 @@
             // 
             // comBoxFrequency
             // 
+            this.comBoxFrequency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comBoxFrequency.FormattingEnabled = true;
             this.comBoxFrequency.Items.AddRange(new object[] {
-            "Full Report",
-            "Monthly Report",
-            "Weekly Report"});
-            this.comBoxFrequency.Location = new System.Drawing.Point(431, 433);
-            this.comBoxFrequency.Margin = new System.Windows.Forms.Padding(4);
+            "FullReport",
+            "MonthlyReport",
+            "WeeklyReport"});
+            this.comBoxFrequency.Location = new System.Drawing.Point(323, 352);
             this.comBoxFrequency.Name = "comBoxFrequency";
-            this.comBoxFrequency.Size = new System.Drawing.Size(160, 24);
+            this.comBoxFrequency.Size = new System.Drawing.Size(121, 21);
             this.comBoxFrequency.TabIndex = 2;
+            this.comBoxFrequency.SelectedIndex = 0;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblMessage);
             this.panel1.Controls.Add(this.listDebts);
-            this.panel1.Location = new System.Drawing.Point(16, 15);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1020, 411);
+            this.panel1.Size = new System.Drawing.Size(765, 334);
             this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Message:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMessage.Location = new System.Drawing.Point(220, 293);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(283, 24);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // CreateFinancialReport
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.comBoxFrequency);
             this.Controls.Add(this.btnGenerate);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CreateFinancialReport";
             this.Text = "Create Financial Report";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -134,5 +154,7 @@
         private System.Windows.Forms.ColumnHeader amtHeader;
         private System.Windows.Forms.ColumnHeader nextPayHeader;
         private System.Windows.Forms.ColumnHeader lastPayHeader;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label1;
     }
 }
---
+++ MyFinancialApp/DTOs/Responses/FinancialReportResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MyFinancialApp.DTOs.Data;
+
+namespace MyFinancialApp.DTOs.Responses
+{
+    public class FinancialReportResponse
+    {
+        public List<DebtEntry> Debts { get; set; } = new List<DebtEntry>();
+
+        public List<string> Notices { get; set; } = new List<string>();
+    }
+}
